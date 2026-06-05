2778154d2158bd6c31a82549aa8d1a8c376f6975
Nakul <nakulkhandelwal18@gmail.com>
2026-06-05 17:28:41 +0530

    Fix hero image filename casing for Vercel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,11 +12,11 @@ import WhyChooseMaySecret from '../components/WhyChooseMaySecret';
 import Newsletter from '../components/Newsletter';
 import GlassSkinRoutine from '../components/GlassSkinRoutine';
 import { BRAND_NAME } from '../config/brand';
-import heroImg1 from '../assets/images/heroimg1.PNG';
-import heroImg2 from '../assets/images/heroimg2.PNG';
-import heroImg3 from '../assets/images/heroimg3.PNG';
-import heroImg4 from '../assets/images/heroimg4.PNG';
-import heroImg5 from '../assets/images/heroimg5.PNG';
+import heroImg1 from '../assets/images/heroimg1.png';
+import heroImg2 from '../assets/images/heroimg2.png';
+import heroImg3 from '../assets/images/heroimg3.png';
+import heroImg4 from '../assets/images/heroimg4.png';
+import heroImg5 from '../assets/images/heroimg5.png';
 
 const Home: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
